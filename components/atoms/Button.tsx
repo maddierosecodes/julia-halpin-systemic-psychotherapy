97f805b576e227e03342eaeb577698e920c61c44
Madeleine Hughes <70583506+maddierosecodes@users.noreply.github.com>
2025-05-13 17:22:06 +0100

    Copy Updates 2

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -15,11 +15,16 @@ export const Button = ({
   className = "",
   ...props
 }: ButtonProps) => {
-  const baseStyles = `relative transform skew-x-[20deg] inline-block font-main text-md font-bold shadow-[5px_5px_0px_0px_rgba(198,166,100,0.5)] transition-all duration-500 group cursor-pointer border-2 border-gold-500 overflow-hidden ${
-    darkBg
+  const baseStyles = `relative transform skew-x-[20deg] inline-block font-main text-md font-bold shadow-[5px_5px_0px_0px_rgba(198,166,100,0.5)] transition-all duration-500 group cursor-pointer border-2 border-gold-500 overflow-hidden`;
+
+  const variantStyles = {
+    primary: darkBg
       ? "text-lavender-100 bg-indigo-700 before:bg-indigo-500/90 hover:text-lavender"
-      : "text-lavender-800 bg-indigo-700/20 before:bg-indigo-700/30 hover:text-lavender-100"
-  } before:content-[''] before:absolute before:top-0 before:left-[-100%] before:w-full before:h-full before:transition-all before:duration-500 hover:before:left-0 before:z-[-1]`;
+      : "text-lavender-800 bg-indigo-700/20 before:bg-indigo-700/30 hover:text-lavender-100",
+    secondary: darkBg
+      ? "text-gold-500 bg-indigo-900 before:bg-indigo-700/90 hover:text-gold-300"
+      : "text-gold-700 bg-gold-500/20 before:bg-gold-500/30 hover:text-gold-900",
+  };
 
   const sizeStyles = {
     small: "px-4 py-1.5",
@@ -29,7 +34,7 @@ export const Button = ({
 
   return (
     <button
-      className={`${baseStyles} ${sizeStyles[size]} ${className}`}
+      className={`${baseStyles} ${variantStyles[variant]} ${sizeStyles[size]} ${className}`}
       {...props}
     >
       <span className="transform skew-x-[-20deg] inline-block relative z-10">
